69cac3297bd1e95218f6dc7749cd6e3091f28145
Alex Ermolaev <alex191099@gmail.com>
2019-11-20 03:02:10 +0200

    run.sh

--- src/main/java/io/battlesnake/starter/Logic.java
+++ src/main/java/io/battlesnake/starter/Logic.java
@@ -10,44 +10,37 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Logic {
-//    final int height;
-//    final int width;
-//    final ArrayList<Point> food;
-//    final ArrayList<Snake> snakes;
-//    final Snake me;
+    final int height;
+    final int width;
+    final ArrayList<Point> food;
+    final ArrayList<Snake> snakes;
+    final Snake me;
 
     Logic(String json) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("/var/log/file.txt"));
-            writer.write(json + "\n");
+        System.out.println(json);
+        JSONObject object = new JSONObject(json);
 
-//        JSONObject object = new JSONObject(json);
-//
-//        JSONObject board = object.getJSONObject("board");
-//        height = board.getInt("height");
-//        width = board.getInt("width");
-//        food = new ArrayList<>();
-//        JSONArray food = board.getJSONArray("food");
-//        for (int i = 0; i < food.length(); i++) {
-//            JSONObject point = food.getJSONObject(i);
-//            int x = point.getInt("x");
-//            int y = point.getInt("y");
-//            this.food.add(new Point(x, y));
-//        }
-//        snakes = new ArrayList<>();
-//        JSONArray snakes = board.getJSONArray("snakes");
-//        for (int i = 0; i < snakes.length(); i++) {
-//            Snake snake = new Snake(snakes.getJSONObject(i));
-//            this.snakes.add(snake);
-//        }
-//
-//        me = new Snake(object.getJSONObject("you"));
-//        writer.write(me.toString() + "\n\n");
-//        for (Snake snake : this.snakes)
-//            writer.write(snake.toString() + "\n");
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        JSONObject board = object.getJSONObject("board");
+        height = board.getInt("height");
+        width = board.getInt("width");
+        food = new ArrayList<>();
+        JSONArray food = board.getJSONArray("food");
+        for (int i = 0; i < food.length(); i++) {
+            JSONObject point = food.getJSONObject(i);
+            int x = point.getInt("x");
+            int y = point.getInt("y");
+            this.food.add(new Point(x, y));
         }
+        snakes = new ArrayList<>();
+        JSONArray snakes = board.getJSONArray("snakes");
+        for (int i = 0; i < snakes.length(); i++) {
+            Snake snake = new Snake(snakes.getJSONObject(i));
+            this.snakes.add(snake);
+        }
+
+        me = new Snake(object.getJSONObject("you"));
+        System.out.println(me.toString() + "\n");
+        for (Snake snake : this.snakes)
+            System.out.println(snake);
     }
 }
